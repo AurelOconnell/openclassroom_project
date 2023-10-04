4568c7a3bdc61a267849a3eea6c815149a434356
AurelOconnell <aurelienoconnell@gmail.com>
2023-10-04 11:15:53 +0200

    building component

---
+++ src/app/models/share-pics.models.ts
@@ -0,0 +1,4 @@
+export class SharePics {
+    title!: string;
+
+}
--- src/app/share-pics/share-pics.component.ts
+++ src/app/share-pics/share-pics.component.ts
@@ -11,12 +11,24 @@ export class SharePicsComponent implements OnInit{
   createdDate!: Date;
   likes!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit(): void {
       this.title = 'Un Chapeau';
       this.description = 'Un beau chapeau';
       this.createdDate = new Date;
-      this.likes = 8;
+      this.likes = 0;
       this.imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/3916Chapeau_tendu.png/1200px-3916Chapeau_tendu.png';
+      this.buttonText = "This pic!!";
+  }
+
+  onLike() {
+    if(this.buttonText === 'This pic!!') {
+      this.likes++;
+      this.buttonText = 'Nay!'
+    } else {
+      this.likes--;
+      this.buttonText = 'This pic!!';
+    }
   }
 }
