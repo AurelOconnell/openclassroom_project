b181978edc8765470663c5d11c3cdac5038ca395
AurelOconnell <aurelienoconnell@gmail.com>
2023-10-04 16:57:40 +0200

    ngStyle & ngClass to bring some SCSS style on the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,14 +16,14 @@ export class AppComponent implements OnInit {
         title: 'La Savoie',
         description: 'Carte de la Savoie',
         createdDate: new Date(),
-        likes: 3,
+        likes: 100,
         imageUrl: 'https://www.larousse.fr/encyclopedie/data/cartes/1300307-Savoie.HD.jpg',
       },
       {
         title: 'Des montagnes',
         description: 'Magnifique lieu de randonnée',
         createdDate: new Date(),
-        likes: 10,
+        likes: 160,
         imageUrl: 'https://media.routard.com/image/83/0/photo.1536830.jpg',
         location: 'Savoie'
       },
@@ -31,30 +31,7 @@ export class AppComponent implements OnInit {
         title: 'Un lac de montagne',
         description: 'Pour se prélasser au bord pendant la rando',
         createdDate: new Date(),
-        likes: 12,
-        imageUrl: 'https://www.cieau.com/wp-content/uploads/2022/04/montagne-2.jpg',
-        location: 'Isère'
-      },
-      {
-        title: 'La Savoie',
-        description: 'Carte de la Savoie',
-        createdDate: new Date(),
-        likes: 3,
-        imageUrl: 'https://www.larousse.fr/encyclopedie/data/cartes/1300307-Savoie.HD.jpg',
-      },
-      {
-        title: 'Des montagnes',
-        description: 'Magnifique lieu de randonnée',
-        createdDate: new Date(),
-        likes: 10,
-        imageUrl: 'https://media.routard.com/image/83/0/photo.1536830.jpg',
-        location: 'Savoie'
-      },
-      {
-        title: 'Un lac de montagne',
-        description: 'Pour se prélasser au bord pendant la rando',
-        createdDate: new Date(),
-        likes: 12,
+        likes: 240,
         imageUrl: 'https://www.cieau.com/wp-content/uploads/2022/04/montagne-2.jpg',
         location: 'Isère'
       },
