c013be5ac0c0590586c356ef28cd55ce21189b21
AurelOconnell <aurelienoconnell@gmail.com>
2023-10-06 16:36:22 +0200

    introduction to Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, filter, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 
+  interval$!: Observable<string>;
+
   ngOnInit(): void {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3  === 0), // ordre des opérateurs est important
+      map((value) =>
+        value % 2 === 0 ?
+        'Je suis ' + (value) + ' et je suis pair' : 
+        'Je suis ' + (value) + ' et je suis impair'
+      ),
+      tap(text => this.logger(text))
+    );
+  }
+
+  logger(text: string) {
+    console.log('log: ' + (text));
   }
 }
