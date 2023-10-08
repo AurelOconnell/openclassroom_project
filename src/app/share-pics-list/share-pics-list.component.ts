0e988a65192bb233717bd727e3b454a34d8898ae
AurelOconnell <aurelienoconnell@gmail.com>
2023-10-08 17:00:22 +0200

    destroy an observable

--- src/app/share-pics-list/share-pics-list.component.ts
+++ src/app/share-pics-list/share-pics-list.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SharePics } from '../models/share-pics.models';
 import { SharePicsService } from '../service/share-pics.service';
+import { Subject, interval, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-share-pics-list',
   templateUrl: './share-pics-list.component.html',
   styleUrls: ['./share-pics-list.component.scss'],
 })
-export class SharePicsListComponent implements OnInit {
+export class SharePicsListComponent implements OnInit, OnDestroy {
+
   sharePics!: SharePics[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private sharePicsService: SharePicsService) {}
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.sharePics = this.sharePicsService.getAllSharePics();
+
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+      this.destroy$.next(true);
   }
 }
